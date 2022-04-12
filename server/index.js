fe0d198db3a8564bc7d060d7a62eb70e33b7e16e
hamza-starcevic <starcevic.hamza.21@size.ba>
2022-04-12 12:32:51 +0200

    Enabled ndoemon

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
-const express = require('express')
-const { default: mongoose } = require('mongoose')
-const app = express()
-const port = 3001
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const app = express();
+const port = 3001;
 
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
