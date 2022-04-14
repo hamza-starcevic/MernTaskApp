333c924eb857d64bd1181189cb1d20e74a6625db
hamza-starcevic <starcevic.hamza.21@size.ba>
2022-04-15 00:14:53 +0200

    Set up back end completely

--- server/index.js
+++ server/index.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
+const UserModel = require("./models/Users");
 const app = express();
-const port = 3001;
 
-mongoose.connect("mongodb+srv://Hamza:kenansin2002@cluster0.xs5dn.mongodb.net/BazaPodatakaRoditelji?retryWrites=true&w=majority")
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://Hamza:kenansin2002@cluster0.xs5dn.mongodb.net/BazaPodatakaRoditelji?retryWrites=true&w=majority"
+);
+
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      console.error(err);
+    }
+    res.json(result);
+  });
+});
+
+app.post("/createUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
+});
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(3001, () => console.log(`Example app listening on port 3001!`));
---
+++ server/models/Users.js
@@ -0,0 +1,22 @@
+const { default: mongoose } = require("mongoose");
+
+mongoose
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserModel = mongoose.model("users", UserSchema);
+
+module.exports = UserModel;
