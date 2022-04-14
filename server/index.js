61ad3b7eeb8005e6a0707e0f5a91fdbc2e0bbbff
hamza-starcevic <starcevic.hamza.21@size.ba>
2022-04-14 02:10:47 +0200

    Added connection to database

--- server/index.js
+++ server/index.js
@@ -3,5 +3,7 @@ const { default: mongoose } = require("mongoose");
 const app = express();
 const port = 3001;
 
+mongoose.connect("mongodb+srv://Hamza:kenansin2002@cluster0.xs5dn.mongodb.net/BazaPodatakaRoditelji?retryWrites=true&w=majority")
+
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
